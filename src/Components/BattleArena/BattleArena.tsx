7bd0189d3d2bc0fee0dfab73170fc2f656950cbc
Hrant Grishyan <hrant_grishyan@epam.com>
2024-03-09 17:52:19 +0400

    disable eslint

--- src/Components/BattleArena/BattleArena.tsx
+++ src/Components/BattleArena/BattleArena.tsx
@@ -39,6 +39,7 @@ const BattleArena = ({
         if(isBattleOver) {
             addStatistics(player.score > 0);
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isBattleOver, player.score]);
 
     return(<div className={styles.container}>
